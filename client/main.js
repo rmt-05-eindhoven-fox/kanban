15627c1f7af2ae26909b5fa7e851fce46e24715c
Hisyam-Khowarik <hisyamkhowarik@gmail.com>
2020-11-05 00:27:10 +0700

    1. Add CRUD task 2. Add Api-document 3. Add Vue masih belum jadi dan masih memahami lagi

---
+++ client/main.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        pageName: 'login',
+        user:{
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        
+    }
+})
--- server/routers/task.js
+++ server/routers/task.js
@@ -2,12 +2,12 @@ const exppress = require('express')
 const router = exppress.Router()
 const TasksController = require('../controllers/TasksController')
 const authentication = require('../middlewares/authentication')
-const Authorization = require('../middlewares/authorization')
+const authorization = require('../middlewares/authorization')
 
 router.get('/', TasksController.list)
 router.use(authentication)
 router.post('/', TasksController.addTask)
-router.put('/:id', TasksController.updateTask)
-router.delete('/:id', TasksController.deleteTask)
+router.put('/:id', authorization,TasksController.updateTask)
+router.delete('/:id', authorization, TasksController.deleteTask)
 
 module.exports = router
