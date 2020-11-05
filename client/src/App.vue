189417a59e42c569cf89ff238a4976df8c6ed5ea
RamaRamesses <maulanaribrahim@outlook.com>
2020-11-05 21:36:04 +0700

    fixed vue.js landingpage not showing up

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <!-- <taskpage></taskpage> -->
-        <landingpage></landingpage>
+        <taskpage @logout="logout" v-if="targetPage === 'task-page'" :dataTasks="tasks"></taskpage>
+        <landingpage @register="register" @login="login" v-else-if="targetPage === 'landing-page'"></landingpage>
     </div>
 </template>
 
@@ -17,9 +17,78 @@ export default {
     },
     data() {
         return {
+            base_url: `http://localhost:3000`,
             message: 'Hello world',
+            tasks: {
+                backlogTasks: [],
+                productionTasks: [],
+                doneTasks: [],
+                developmentTasks: []
+            },
+            targetPage: '',
         };
     },
+    methods: {
+        login(payload) {
+            axios.post(`${this.base_url}/login`, {
+                email: payload.email,
+                password: payload.password 
+            })
+            .then(response => {
+                localStorage.setItem('access_token', response.data.access_token);
+                this.targetPage = 'task-page'
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        },
+        register(payload) {
+            let user = this.newUser;
+            axios.post(`${this.base_url}/register`, {
+                email: payload.email,
+                password: payload.password
+            })
+            .then(response => {
+                console.log(response.data);
+                let container = document.getElementById('container')
+                container.classList.remove("right-panel-active");
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        },
+        logout(value){
+            this.targetPage = value;
+        }
+    },
+    created() {
+        axios.get(`${this.base_url}/tasks`, {
+                headers: {access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJtYW5zdXJAeWFob28uYWMiLCJpYXQiOjE2MDQ1NzQ5MTl9.lJ71tV-eVX-aC1KjDa3cFzYrIgqayqAbB1xXIdfzb8o'}
+            })
+            .then(response => {
+                response.data.forEach((el) => {
+                    if(el.category === 'backlog'){
+                        this.tasks.backlogTasks.push(el);
+                    } else if (el.category === 'production'){
+                        this.tasks.productionTasks.push(el);
+                    } else if (el.category === 'development'){
+                        this.tasks.developmentTasks.push(el);
+                    } else {
+                        this.tasks.doneTasks.push(el);
+                    }
+                })
+            })
+            .catch(error => {
+                this.tasks = error.response;
+        });
+        let access_token = localStorage.getItem('access_token')
+        if(access_token){
+            this.targetPage = 'task-page';
+        } else {
+            this.targetPage = 'landing-page';
+        }
+    }
 };
 </script>
 
--- client/src/components/taskpage.vue
+++ client/src/components/taskpage.vue
@@ -6,65 +6,22 @@
                         <header style="background-color: #30BFBF;" class="card-header">
                         <h2>Backlog</h2>
                         </header>
-                        <div class="task-body"> 
-                            <div class="element-body">
-                                <p>New Account Creation Flow</p>
+                        <div class="task-body">
+                            <div class="element-body" v-for="(task, i) in dataTasks.backlogTasks" :key="i">
+                                <p contenteditable="true">{{task.title}}</p>
+                                <p>{{task.description}}</p>
                                 <div class="card-author">
                                     <a class="author-avatar" href="#">
                                     <img src="avatar.png">
                                     </a>
                                     <div class="author-name">
-                                        <span class="badge badge-danger">Server</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="element-body">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, dolorum.</p>
-                                <div class="card-author">
-                                    <a class="author-avatar" href="#">
-                                    <img src="avatar.png">
-                                    </a>
-                                    <div class="author-name">
-                                        <span class="badge badge-danger">Server</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="element-body">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, dolorum.</p>
-                                <div class="card-author">
-                                    <a class="author-avatar" href="#">
-                                    <img src="avatar.png">
-                                    </a>
-                                    <div class="author-name">
-                                        <span class="badge badge-danger">Server</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="element-body">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, dolorum.</p>
-                                <div class="card-author">
-                                    <a class="author-avatar" href="#">
-                                    <img src="avatar.png">
-                                    </a>
-                                    <div class="author-name">
-                                        <span class="badge badge-danger">Server</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="element-body">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, dolorum.</p>
-                                <div class="card-author">
-                                    <a class="author-avatar" href="#">
-                                    <img src="avatar.png">
-                                    </a>
-                                    <div class="author-name">
-                                        <span class="badge badge-danger">Server</span>
+                                        <span class="badge badge-danger">{{task.description}}</span>
                                     </div>
                                 </div>
                             </div>
                             <form>
                                 <button type="button" class="btn btn-default btn-sm">
-                                    <i class="fas fa-plus"></i> Add Task
+                                    <span class="glyphicon">&#x2b;</span> Add Task
                                 </button>
                             </form>
                         </div>
@@ -85,20 +42,9 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="element-body">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, dolorum.</p>
-                                <div class="card-author">
-                                    <a class="author-avatar" href="#">
-                                    <img src="avatar.png">
-                                    </a>
-                                    <div class="author-name">
-                                        <span class="badge badge-danger">Server</span>
-                                    </div>
-                                </div>
-                            </div>
                             <form>
                                 <button type="button" class="btn btn-default btn-sm">
-                                    <i class="fas fa-plus"></i> Add Task
+                                    <span class="glyphicon">&#x2b;</span> Add Task
                                 </button>
                             </form>
                         </div>
@@ -119,20 +65,9 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="element-body">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, dolorum.</p>
-                                <div class="card-author">
-                                    <a class="author-avatar" href="#">
-                                    <img src="avatar.png">
-                                    </a>
-                                    <div class="author-name">
-                                        <span class="badge badge-danger">Server</span>
-                                    </div>
-                                </div>
-                            </div>
                             <form>
                                 <button type="button" class="btn btn-default btn-sm">
-                                    <i class="fas fa-plus"></i> Add Task
+                                    <span class="glyphicon">&#x2b;</span> Add Task
                                 </button>
                             </form>
                         </div>
@@ -142,23 +77,27 @@
                         <h2>Done</h2>
                         </header>
                         <div class="task-body"> 
-                            <div class="element-body">
-                                <p>New Account Creation Flow</p>
+                            <div class="element-body" v-for="(task, i) in dataTasks.doneTasks" :key="i">
+                                <p contenteditable="true">{{task.title}}</p>
+                                <p>{{task.description}}</p>
+                                    <button type="button" class="btn btn-primary">Edit</button>
+                                    <button type="button" class="btn btn-danger"><span class="glyphicon">&#xe020;</span></button>
+                                    <button type="button" class="btn btn-warning">></button>
                                 <div class="card-author">
                                     <a class="author-avatar" href="#">
                                     <img src="avatar.png">
                                     </a>
                                     <div class="author-name">
-                                        <span class="badge badge-danger">Server</span>
+                                        Japra
+                                        <span class="badge badge-success">{{task.category}}</span>
                                     </div>
-                                </div>
                             </div>
-                            <form>
-                                <button type="button" class="btn btn-default btn-sm">
-                                    <i class="fas fa-plus"></i> Add Task
-                                </button>
-                            </form>
                         </div>
+                        <form>
+                            <button type="button" class="btn btn-default btn-sm">
+                                <span class="glyphicon">&#x2b;</span> Add Task
+                            </button>
+                        </form>
                     </article>
                 </section>
     </div>
@@ -171,9 +110,162 @@ export default {
         return {
         };
     },
+    methods: {
+        logout(){
+            localStorage.removeItem('access_token');
+            this.$emit('logout', 'landing-page');
+        }
+    },
+    props: ['dataTasks'],
+    created() {
+
+    }
 };
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400;500&display=swap');
 
+body {
+    padding: 0;
+    margin: 0;
+    font-family: 'DM Mono', monospace;
+}
+
+.card {   
+    align-self: start;
+
+    min-height: 650px;
+    max-height: 750px;
+    min-width: 350px;
+    border-radius: 16px;
+    background: #EAEEEF;
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 
+              0 10px 10px rgba(0,0,0,0.22);
+    transition: .2s;
+    margin-left: -15px;
+
+}
+
+.task-body {
+    display:grid;
+    padding: 1.5rem;
+    max-height: 750px;
+    overflow-y: scroll;
+    align-self: start;
+    margin-bottom: 50px;
+}
+
+.task-body form button {
+    background-color: #00AAFF;
+}
+
+.card-header {
+    background-color: #00AAFF;
+    padding: 15px 10px;
+    color: white;
+    text-align: center;
+}
+
+.element-body {
+    padding: 15px;
+    margin: 10px;
+    background-color: #FFFFFF;
+    border-radius: 15px;
+}
+
+.card-author {
+    /* margin: 3rem 0 0; */
+    display: grid;
+    margin-top: 5px;
+    grid-template-columns: 35px 45px 35px;
+    align-items: center;
+    position: relative;
+}
+
+.task-badge {
+    font-style: normal;
+    font-weight: 700;
+    font-size: 10px;
+    color: #7a7a8c;
+    
+}
+
+.author-avatar {
+    grid-area: auto;
+    /* align-self: start; */
+    position: relative;
+    box-sizing: border-box;
+}
+
+.author-avatar img {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    filter: grayscale(100%);
+    display: block;
+    overflow: hidden;
+    /* margin: 16px 5px; */
+}
+
+.author-name {
+    grid-area: auto;
+    box-sizing: border-box;
+    font-size: 13px;
+}
+
+.author-name-prefix {
+    font-style: normal;
+    font-weight: 700;
+    font-size: 10px;
+    color: #7a7a8c;
+}
+
+.card-list {
+    display: flex;
+    padding: 100px;
+}
+
+.card-list::-webkit-scrollbar {
+    width: 10px;
+    height: 10px;
+}
+.card-list::-webkit-scrollbar-thumb {
+    background: #201c29;
+    border-radius: 10px;
+    box-shadow: inset 2px 2px 2px hsla(0,0%,100%,.25), inset -2px -2px 2px rgba(0,0,0,.25);
+}
+
+.card-list::-webkit-scrollbar-track {
+    background: linear-gradient(90deg,#201c29,#201c29 1px,#17141d 0,#17141d);
+}
+
+.card:focus-within~.card, .card:hover~.card {
+    transform: translateX(10px);
+}
+
+.card:hover {
+    transform: translateY(-1rem);
+}
+
+.card:not(:first-child) {
+    /* margin-left: -100px; */
+}
+
+.container div .card {
+    margin-top: 65px;
+    justify-items: center;
+    display: grid;
+    padding: 10px;
+    box-sizing: border-box;
+}
+
+.container div .card-body {
+    display: flex;
+    font-family: 'Noto Sans', sans-serif;
+    padding: 10px 15px;
+    margin: 5px 25px;
+    background-color: #FFFFFF;
+    border-radius: 15px;
+}
 </style>
