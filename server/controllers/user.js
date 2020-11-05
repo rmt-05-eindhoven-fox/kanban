8d7013c425c14487b345dd5330a14862186b6262
Muhammad Nur Firdaus Setiadi <mnfirdauss@hotmail.com>
2020-11-05 20:07:55 +0700

    done until register and login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,45 +1,47 @@
-const { User, Task } = require("../models")
-const { comparePassword } = require("../helpers/bcrypt")
-const { generateToken } = require("../helpers/jwt")
+const { User, Task } = require("../models");
+const { comparePassword } = require("../helpers/bcrypt");
+const { generateToken } = require("../helpers/jwt");
 
 class UserController {
-	static async register (req, res, next) {
-		const { username, email, password } = req.body
+  static async register(req, res, next) {
+    const { username, email, password } = req.body;
+    try {
+      const payload = {
+        username,
+        email,
+        password,
+      };
 
-		try {
-			const payload = {
-				username, email, password
-			}
+      const newUser = await User.create(payload);
+      res.status(201).json(newUser);
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
 
-			const newUser = await User.create(payload)
-			res.status(201).json(newUser)
-		} catch (err) {
-			next(err)
-		}
-	}
+  static async login(req, res, next) {
+    const { email, password } = req.body;
 
-	static async login (req, res, next) {
-		const { username, email, password } = req.body
+    try {
+      const payload = {
+        email,
+        password,
+      };
 
-		try {
-			const payload = {
-				email, password
-			}
-
-			const user = await User.findOne({ where: { email } })
-			if (!user) {
-				throw { msg: 'Wrong email/password', status: 400 }
-			} else if (!comparePassword(password, user.password)) {
-				throw { msg: 'Wrong email/password', status: 400 }
-			} else {
-				const access_token = generateToken(email)
-				res.status(200).json({ access_token })
-			}	
-		} catch (err) {
-			console.log(err);
-			next(err)
-		}
-	}
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        throw { msg: "Wrong email/password", status: 400 };
+      } else if (!comparePassword(password, user.password)) {
+        throw { msg: "Wrong email/password", status: 400 };
+      } else {
+        const access_token = generateToken(email);
+        res.status(200).json({ access_token });
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
-module.exports = UserController
+module.exports = UserController;
