b54cf648c2ca6fd605eb1a1b20756ac733331385
RamaRamesses <maulanaribrahim@outlook.com>
2020-11-05 16:01:22 +0700

    Added new html design

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <Login></Login>
+    </div>
+</template>
+
+<script>
+import Login from './Login'
+export default {
+    name: 'App',
+    components: {
+        Login
+    },
+    data() {
+        return {
+            message: 'Hello world',
+        };
+    },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data() {
+        return {
+            message: 'Hello world',
+        };
+    },
+};
+</script>
+
+<style scoped>
+</style>
