0b984e33a2e40cb1b4a7550c28c1cfaf7386b7f1
Nikolas Stefano <nz.stefano@gmail.com>
2020-11-06 16:14:51 +0700

    add config

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const app = express();
 const routes = require("./routes");
 const errorHandler = require("./middlewares/errorHandler");
