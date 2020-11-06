db1b38b8d069343996e0e3415c638efd422a590f
delviawp <delviawp@gmail.com>
2020-11-07 02:03:02 +0800

    add port

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000 || process.env.port
+const port = process.env.PORT || 3000
 const route = require('./routes/index')
 const cors = require('cors')
 const errorHandler = require('./middlewares/errorHandler')
