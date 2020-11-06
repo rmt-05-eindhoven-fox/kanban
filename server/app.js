a3d1d575e332558e0b0b80c32e671865fade6e66
rizky yoditia <rizkyodit@gmail.com>
2020-11-06 20:13:35 +0700

    fix port

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
-const port = 3000
+const port = process.env.PORT || 3000
 const app = express()
 const router = require('./router/index')
 const errorHandler = require('./middlewares/errorHandling')
