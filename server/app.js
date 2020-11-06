391b37545b4cf36eb9feeea047752bbd02ffb7a0
nadivaramadhani <alwafi.nadiva@gmail.com>
2020-11-06 20:31:55 +0700

    bismillah

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
-
+if (process.env.NODE_ENV != 'production') {
+    require('dotenv').config();
+}
+// require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
 const router = require('./routers/index');
