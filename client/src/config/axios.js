404483ec7577f8934ccd8f6cf586dc6a776da255
0x67 <gabri.putra@gmail.com>
2020-11-06 17:07:46 +0700

    Added deploy config

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/'
+    baseURL: 'https://young-refuge-86223.herokuapp.com/'
 })
-
+// http://localhost:3000/
 export default instance
