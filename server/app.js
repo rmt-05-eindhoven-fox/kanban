541304ff120e06ad92f547d8c58c96252eab3378
amandajehan <amandajehan@gmail.com>
2020-11-07 01:45:42 +0700

    re-push updated codes e.g: googlelogin, PATCH edit method using Vue draggable etc)

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const routes = require("./routes/index.js");
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const errorHandler = require("./middlewares/errorHandler.js");
 
 app.use(cors());
