f417b673be38dc652c73222ed64ffb19a0708793
Agung Adi Prasetio <prasetioxagung@gmail.com>
2020-11-07 00:38:16 +0700

    add db production, add start di package.json, add routing get / di app.js, handle dotenv

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
-require('dotenv').config()
+if(process.env.NODE_ENV != 'production'){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT
@@ -9,7 +12,7 @@ const errorHandler = require('./middlewares/error-handler')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+app.get('/', (req, res)=> res.status(200).json({msg: 'Welcome'}))
 app.use(router)
 app.use(errorHandler)
 
