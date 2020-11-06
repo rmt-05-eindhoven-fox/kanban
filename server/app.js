411f3a788e80ddb38ebfa2e52c0ddc8ef8184bbd
Gerry Anthony <gerrysimangunsong@gmail.com>
2020-11-06 23:57:31 +0700

    semoga jadi

--- server/app.js
+++ server/app.js
@@ -6,12 +6,12 @@ const cors = require('cors');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get('/', (req, res) => res.status(200).json({ message: 'Hello World!' }));
+app.get('/', (req, res) => {res.status(200).json({ message: 'Hello World!' })});
 app.use(router);
 
 app.use(errorHandler);
--- server/helpers/hash.js
+++ server/helpers/hash.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 
 const hashPassword = (password) => {
-  const salt = bcrypt.genSaltSync(+process.env.SALT);
+  const salt = bcrypt.genSaltSync(10);
   return bcrypt.hashSync(password, salt);
 }
 
