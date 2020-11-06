416025a7789dbf2f5ca6dc663a5e8569cc1231bd
Bintang Wibawa Mukti <bintang.wib@gmail.com>
2020-11-06 19:01:41 +0700

    deploy to heroku - fix npm start

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const router  = require('./routes/index')
 const errorHandler = require('./middlewares/errorHandling')
 
