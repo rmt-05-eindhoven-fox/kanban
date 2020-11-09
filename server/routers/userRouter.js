31e8dd679c49d84439433fd08d1df65c6fc2185e
Muhammad Ayas <mayyasy@gmail.com>
2020-11-09 16:32:08 +0700

    fixing gitignore

--- server/routers/userRouter.js
+++
@@ -1,9 +0,0 @@
-  
-const router = require('express').Router()
-const UserController = require('../controllers/userController')
-
-router.post('/register', UserController.register)
-router.post('/login', UserController.login)
-router.post('/googleLogin', UserController.googleLogin)
-
-module.exports = router;
