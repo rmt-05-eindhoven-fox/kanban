52e0fb4c971a9b21b21a6653c744e0b088f16ae0
erick <erick.gunawan70@gmail.com>
2020-11-20 12:47:26 +0700

    deploying

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 8080
 const task = require('./routes/task')
 const user = require('./routes/user')
 const errorHandler = require("./middlewares/errorHandler")
